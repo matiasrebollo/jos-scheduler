e5f74990f1d03020fc3747eef3e74338b857e739
Marcos <mgarcian@fi.uba.ar>
2024-10-19 16:04:15 -0300

    implementando cambios de contexto

--- sched/kern/env.c
+++ sched/kern/env.c
@@ -510,7 +510,14 @@ env_run(struct Env *e)
 	//	and make sure you have set the relevant parts of
 	//	e->env_tf to sensible values.
 	// Your code here
+	if (curenv && curenv->env_status == ENV_RUNNING)
+		curenv->env_status = ENV_RUNNABLE;
+
 	curenv = e;
+	curenv->env_status = ENV_RUNNING;
+	curenv->env_runs++;
+
+	env_load_pgdir(e);
 
 	// Needed if we run with multiple procesors
 	// Record the CPU we are running on for user-space debugging
--- sched/kern/init.c
+++ sched/kern/init.c
@@ -69,8 +69,6 @@ i386_init(void)
 #else
 	// Touch all you want.
 	ENV_CREATE(user_hello, ENV_TYPE_USER);
-	ENV_CREATE(user_hello, ENV_TYPE_USER);
-	ENV_CREATE(user_hello, ENV_TYPE_USER);
 #endif  // TEST*
 
 	// Eliminar esta llamada una vez completada la parte 1
@@ -78,7 +76,7 @@ i386_init(void)
 	env_run(&envs[0]);
 
 	// Schedule and run the first user environment!
-	// sched_yield();
+	//sched_yield();
 }
 
 // While boot_aps is booting a given CPU, it communicates the per-core
